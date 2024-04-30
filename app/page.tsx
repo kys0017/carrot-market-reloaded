dccc0d623aa8f91f605f3f08860137bba3fa948c
kys0017 <kys0017@gmail.com>
2024-05-01 01:39:29 +0900

    ✨ State Modifiers - 자식 선택자, has

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,20 @@ export default function Home() {
       className="flex h-screen items-center justify-center bg-gray-100 p-5
     sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100"
     >
-      <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg md:flex-row ">
+      <div
+        className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg ring 
+        ring-transparent
+        transition-shadow *:outline-none has-[.peer]:bg-green-100
+        has-[:invalid]:ring-red-100
+        md:flex-row
+      "
+      >
         <input
           className="peer
           h-12 w-full rounded-full bg-gray-200
-          pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-orange-500
+          pl-5 ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-orange-500
           invalid:focus:ring-green-500"
-          type="email"
+          type="text"
           required
           placeholder="Email address"
         />
@@ -22,9 +29,8 @@ export default function Home() {
         <button
           className="
           rounded-full
-          bg-black bg-gradient-to-tr
-          from-cyan-500 to-purple-400 py-2 font-medium text-white 
-           outline-none
+          bg-black
+          py-2 font-medium text-white 
            transition-transform active:scale-90 md:px-10
         "
         >
