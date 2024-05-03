81f0cc1ac7858e811dbe0c3be73d0edcd9646d00
kys0017 <kys0017@gmail.com>
2024-05-04 01:28:17 +0900

    💄 Home Screen - home 화면 마크업, 스타일 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
-      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
-        <div className="group flex flex-col">
-          <input
-            className="w-full bg-gray-100"
-            placeholder="Write your email"
-          />
-          <span className="hidden group-focus-within:block">
-            Make sure it is a valid email...
-          </span>
-          <a href="#">aaaa</a>
-          <button className="btn">Submit</button>
+    <div className="flex min-h-screen flex-col items-center justify-between p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex w-full flex-col items-center gap-3">
+        <Link
+          href="/create-account"
+          className="w-full rounded-md bg-orange-500 py-2.5 text-center text-lg font-medium text-white transition-colors hover:bg-orange-400"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
