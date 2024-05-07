806148bcccd1c9dbdc49baa631f973545fe482f8
kys0017 <kys0017@gmail.com>
2024-05-08 00:02:45 +0900

    🥅 Validation Errors - form object validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,7 +1,12 @@
 "use server";
 import { z } from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+});
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -11,5 +16,14 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get("confirm_password"),
   };
 
-  usernameSchema.parse(data.username);
+  /**
+   * 유효성 검사 실패 시
+   * parse -> throw error. So, try catch 로 잡아야 함.
+   * safeParse -> not throw error. return validation result.
+   */
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    // flatten() - error 객체에서 필요한 것만 표시해 줌.
+    return result.error.flatten();
+  }
 }
