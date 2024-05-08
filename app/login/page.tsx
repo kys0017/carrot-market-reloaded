076e1f8faa049384e2a6bd0e3944e13b525dfac2
kys0017 <kys0017@gmail.com>
2024-05-09 00:30:57 +0900

    🛂 Log In Validation - login form validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,11 @@ import FormBtn from "@/components/button";
 import FormInput from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
-import { onSubmit } from "./actions";
+import { login } from "./actions";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
 
 export default function Login() {
-  const [state, action] = useFormState(onSubmit, null);
+  const [state, dispatch] = useFormState(login, null);
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -15,13 +16,21 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={action} className="flex flex-col gap-3">
-        <FormInput name="email" type="email" placeholder="Email" required />
+      <form action={dispatch} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
         />
         <FormBtn text="Log in" />
       </form>
