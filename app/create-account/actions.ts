f8facd5edf5be9e70f6377416ae6b5ea5167e0a1
kys0017 <kys0017@gmail.com>
2024-05-08 19:27:31 +0900

    🥅 Refinement - refine 사용해서 form object 및 field custom validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,35 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username?",
+      })
+      .min(3, "Way too short!!!")
+      .max(10, "That is too loooooong!")
+      .refine(checkUsername, "no potatoes allowed"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  // .refine(checkPasswords, "Both passwords should be the same!");
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -24,6 +47,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     // flatten() - error 객체에서 필요한 것만 표시해 줌.
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
