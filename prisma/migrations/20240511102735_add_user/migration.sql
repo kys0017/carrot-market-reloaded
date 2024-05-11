d64886378bcd39482cb32df7fbe01643e2f7eae1
kys0017 <kys0017@gmail.com>
2024-05-11 23:48:57 +0900

    🗃️ Relationships - SMSToken model schema, user relation

--- prisma/migrations/20240511102735_add_user/migration.sql
+++ prisma/migrations/20240511102735_add_user/migration.sql
@@ -18,5 +18,17 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
+CREATE TABLE "new_SMSToken" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "token" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "SMSToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_SMSToken" ("created_at", "id", "token", "updated_at", "userId") SELECT "created_at", "id", "token", "updated_at", "userId" FROM "SMSToken";
+DROP TABLE "SMSToken";
+ALTER TABLE "new_SMSToken" RENAME TO "SMSToken";
+CREATE UNIQUE INDEX "SMSToken_token_key" ON "SMSToken"("token");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
