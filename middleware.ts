603983f5d0d68f64c893711945948baba501547a
kys0017 <kys0017@gmail.com>
2024-05-19 20:22:54 +0900

    🐛 Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { MiddlewareConfig, NextRequest } from "next/server";
+import db from "./lib/db";
 
 export async function middleware(req: NextRequest) {
-  console.log("hellooo");
+  /* middleware edge runtime(nodejs 의 경량 버전) 에서 돌아감.
+    사용할 수 있는 npm package 가 적음. = 모든 nodejs API 를 사용할 수 없다.
+    prisma 도 그 중에 하나.
+  */
+  // await db.user.findUnique({});
 }
 
 export const config: MiddlewareConfig = {
