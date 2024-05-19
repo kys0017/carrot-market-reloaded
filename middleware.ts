5840235868daef20eb63a9bd8f99fa6b7d036159
kys0017 <kys0017@gmail.com>
2024-05-19 20:44:01 +0900

    🛂 Authentication Middleware - 페이지 접근 제한

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
-import { MiddlewareConfig, NextRequest } from "next/server";
-import db from "./lib/db";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(req: NextRequest) {
-  /* middleware edge runtime(nodejs 의 경량 버전) 에서 돌아감.
-    사용할 수 있는 npm package 가 적음. = 모든 nodejs API 를 사용할 수 없다.
-    prisma 도 그 중에 하나.
-  */
-  // await db.user.findUnique({});
+  const session = await getSession();
+  const exists = publicOnlyUrls[req.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", req.url));
+    }
+  }
 }
 
 export const config: MiddlewareConfig = {
