ddd80f7f095041921935a9511dd835516ef54d86
kys0017 <kys0017@gmail.com>
2024-05-22 02:01:46 +0900

    ♿️ Github API - access token 으로 요청, user 유무에 따른 처리

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,4 +1,6 @@
-import { notFound } from "next/navigation";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -22,13 +24,52 @@ export async function GET(req: NextRequest) {
       Accept: "application/json",
     },
   });
-  const accessTokenData = await accessToKenResponse.json();
+  const { error, access_token } = await accessToKenResponse.json();
 
-  if ("error" in accessTokenData) {
+  if (error) {
     return new Response(null, {
       status: 400,
     });
   }
 
-  return Response.json({ accessTokenData });
+  const userProfileResponse = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    cache: "no-cache",
+  });
+
+  const { id, avatar_url, login } = await userProfileResponse.json();
+
+  const user = await db.user.findUnique({
+    where: {
+      github_id: id + "",
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (user) {
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    return redirect("/profile");
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      username: login,
+      github_id: id + "",
+      avatar: avatar_url,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const session = await getSession();
+  session.id = newUser.id;
+  await session.save();
+  return redirect("/profile");
 }
