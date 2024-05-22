51a270d72a289b20dd10ca2f0ecec2abee646a70
kys0017 <kys0017@gmail.com>
2024-05-22 19:55:26 +0900

    🚸 SMS Token - user 에 토큰 보내기 전 로직. 기존 토큰 삭제, 새로운 토큰 생성

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import crypto from "crypto";
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
 const phoneSchema = z
   .string()
@@ -18,6 +20,24 @@ interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 export async function smsLogin(prevState: ActionState, formData: FormData) {
   const phone = formData.get("phone");
   const token = formData.get("token");
@@ -26,12 +46,38 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
     // false 이면 action 을 처음 호출했다는 것. = phone 만 입력했다는 것.
     const result = phoneSchema.safeParse(phone);
     if (!result.success) {
-      console.log(result.error.flatten());
       return {
         token: false,
         errors: result.error.flatten(),
       };
     } else {
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            // 해당 유저가 계정을 가지고 있는지 모르기 때문에, 없으면 생성한다.
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"), // random username
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+      // send token using twilio
       return {
         token: true,
       };
