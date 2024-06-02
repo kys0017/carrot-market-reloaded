7044676e86bf87c1257144433d98b6a047af0e24
kys0017 <kys0017@gmail.com>
2024-06-02 20:56:44 +0900

    ✨ PRODUCT UPLOAD Introduction - 상품 추가 버튼 및 페이지 추가, 마크업

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,9 @@
 import ListProduct from "@/components/list-product";
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -34,6 +36,12 @@ export default async function Products() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="fixed bottom-24 right-8 flex size-16 items-center justify-center rounded-full bg-orange-500 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   );
 }
