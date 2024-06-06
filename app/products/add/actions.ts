4e4b99fb8a5f3b9199088dfed839ccd9acf5a9ab
kys0017 <kys0017@gmail.com>
2024-06-06 18:15:57 +0900

    ⚡️ Product Upload - 로컬에 파일 저장 후 db 에 경로 저장, upload form validation 및 useFormState 로 에러 상태 표시

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,11 +1,78 @@
 "use server";
 
-export default async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z
+    .string({
+      required_error: "Photo is required",
+      invalid_type_error: "Photo must be image format",
+    })
+    .min(1, "Photo is required"),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  description: z.string({
+    required_error: "Decription is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+});
+
+export default async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+
+  if (data.photo instanceof File) {
+    if (!data.photo.size) {
+      data.photo = "";
+    } else if (!data.photo.type.startsWith("image/")) {
+      data.photo = null;
+    } else {
+      const photoData = await data.photo.arrayBuffer();
+
+      await fs.appendFile(
+        `./public/${data.photo.name}`,
+        Buffer.from(photoData),
+      );
+      data.photo = `/${data.photo.name}`;
+    }
+  }
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      redirect(`/products/${product.id}`);
+      // redirect("/products");
+    }
+  }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,10 +3,12 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import uploadProduct from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
+  const [state, action] = useFormState(uploadProduct, null);
   const [preview, setPreview] = useState("");
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
@@ -20,9 +22,13 @@ export default function AddProduct() {
     setPreview(url);
   };
 
+  useEffect(() => {
+    if (state?.fieldErrors.photo) setPreview("");
+  }, [state]);
+
   return (
     <div>
-      <form action={uploadProduct} className="flex flex-col gap-5 p-5">
+      <form action={action} className="flex flex-col gap-5 p-5">
         <label
           htmlFor="photo"
           className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 bg-cover bg-center text-neutral-300"
@@ -33,8 +39,13 @@ export default function AddProduct() {
           {preview === "" ? (
             <>
               <PhotoIcon className="w-20" />
-              <div className="text-sm text-neutral-400">
+              <div className="text-center text-sm text-neutral-400">
                 사진을 추가해 주세요.
+                {state?.fieldErrors.photo ? (
+                  <div className="font-medium text-red-500">
+                    {state?.fieldErrors.photo}
+                  </div>
+                ) : null}
               </div>
             </>
           ) : null}
@@ -47,13 +58,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          required
+          placeholder="가격"
+          type="number"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           required
           placeholder="자세한 설명"
           type="text"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
