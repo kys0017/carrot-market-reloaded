6350772e37aa7bb225dcbf409a97b0fc78d32737
kys0017 <kys0017@gmail.com>
2024-06-19 02:05:41 +0900

    💄 See Posts - 동네생활 페이지 마크업

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,65 @@
-export default function Life() {
+import db from "@/lib/db";
+import { formatToTimeAgo } from "@/lib/utils";
+import { ChatBubbleBottomCenterIcon } from "@heroicons/react/24/outline";
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+async function getPost() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+  const posts = await getPost();
+
   return (
-    <div>
-      <h1 className="text-4xl text-white">Life!</h1>
+    <div className="flex flex-col p-5">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="mb-5 flex flex-col gap-2 border-b border-neutral-400 pb-5 text-neutral-400 last:border-b-0 last:pb-0"
+        >
+          <h2 className="text-lg font-semibold text-white">{post.title}</h2>
+          <p>{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-4">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>.</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex items-center gap-4 *:flex *:items-center *:gap-1">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
