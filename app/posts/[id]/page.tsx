87a27f9eeab82d1613e3597f593c2f7d18a6785b
kys0017 <kys0017@gmail.com>
2024-06-19 21:10:34 +0900

    🎨 Likes and Dislikes - 상세 페이지 마크업, 좋아요 동작

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,3 +1,144 @@
-export default function PostDetail() {
-  return <h1>post detail</h1>;
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToTimeAgo } from "@/lib/utils";
+import { EyeIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
+import { revalidatePath } from "next/cache";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getPost(id: number) {
+  try {
+    const post = await db.post.update({
+      where: {
+        id,
+      },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            likes: true,
+          },
+        },
+      },
+    });
+
+    return post;
+  } catch (e) {
+    // 해당 post 가 없으면 prisma 가 에러를 발생.
+    // catch 해서 null 리턴.
+    return null;
+  }
+}
+
+async function getIsLiked(postId: number) {
+  const session = await getSession();
+  const like = await db.like.findUnique({
+    where: {
+      id: {
+        postId,
+        userId: session.id!,
+      },
+    },
+  });
+
+  return Boolean(like);
+}
+
+export default async function PostDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const post = await getPost(id);
+  console.log(post);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const likePost = async () => {
+    "use server";
+    const session = await getSession();
+    try {
+      await db.like.create({
+        data: {
+          postId: id,
+          userId: session.id!,
+        },
+      });
+
+      revalidatePath(`/posts/${id}`);
+    } catch (e) {}
+  };
+
+  const dislikePost = async () => {
+    "use server";
+    const session = await getSession();
+    try {
+      await db.like.delete({
+        where: {
+          id: {
+            postId: id,
+            userId: session.id!,
+          },
+        },
+      });
+
+      revalidatePath(`/posts/${id}`);
+    } catch (error) {}
+  };
+
+  const isLiked = await getIsLiked(id);
+
+  return (
+    <div className="p-5 text-white">
+      <div className="mb-2 flex items-center gap-2">
+        <Image
+          width={28}
+          height={28}
+          className="size-7 rounded-full"
+          src={post.user.avatar!}
+          alt={post.user.username}
+        />
+        <div>
+          <span className="text-sm font-semibold">{post.user.username}</span>
+          <div className="text-xs">
+            <span>{formatToTimeAgo(post.created_at.toString())}</span>
+          </div>
+        </div>
+      </div>
+      <h2 className="text-lg font-semibold">{post.title}</h2>
+      <p className="mb-5">{post.description}</p>
+      <div className="flex flex-col items-start gap-5">
+        <div className="flex items-center gap-2 text-sm text-neutral-400">
+          <EyeIcon className="size-5" />
+          <span>조회 {post.views}</span>
+        </div>
+        <form action={isLiked ? dislikePost : likePost}>
+          <button
+            className={`flex items-center gap-2 rounded-full border border-neutral-400 p-2 text-sm text-neutral-400`}
+          >
+            <HandThumbUpIcon className="size-5" />
+            <span>공감하기 ({post._count.likes})</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
