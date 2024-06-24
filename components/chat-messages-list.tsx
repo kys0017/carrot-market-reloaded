f227ea63fd5455768465e65d5e965a9fdce2857f
kys0017 <kys0017@gmail.com>
2024-06-24 21:52:30 +0900

    🗃️ Code Challenge - 실시간 mesage 를 db 에 저장

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { saveMessage } from "@/app/chats/[id]/actions";
 import { InitialChatMessages } from "@/app/chats/[id]/page";
 import { formatToTimeAgo } from "@/lib/utils";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
@@ -37,7 +38,7 @@ export default function ChatMessagesList({
     setMessage(value);
   };
 
-  const onSubmit = (event: React.FormEvent) => {
+  const onSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setMessages((prevMsgs) => [
       ...prevMsgs,
@@ -67,6 +68,8 @@ export default function ChatMessagesList({
         },
       },
     });
+
+    await saveMessage(message, chatRoomId);
 
     setMessage("");
   };
